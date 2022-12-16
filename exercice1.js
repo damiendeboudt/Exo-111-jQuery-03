e6615c78d4d99e4477c8b1afbfab317c5acbbf26
Damien <damien.deboudt@sfr.fr>
2022-12-16 10:25:49 +0100

    ok

---
+++ exercice1.js
@@ -0,0 +1,7 @@
+let math = $(`#math-heading`);
+math.html(math.html() + `<span>...amazing!</span>`)
+
+let science = $(`#science-heading`);
+science.html(science.html() + `<span>...wow!</span>`)
+
+
